f8eafc214b10486991cb6a0491b423a77ed81446
santxjoya <109623621+santxjoya@users.noreply.github.com>
2025-02-20 19:01:02 -0500

    Update TaskController.js

---
+++ controllers/TaskController.js
@@ -0,0 +1,29 @@
+const Task = require('../models/TaskModel');
+const {Body, validatonResult}=require('express-validator');
+
+const createTask = [
+
+];
+const getAllTasks = [
+
+];
+const getTaskById = [
+
+];
+const updateTask = [
+
+];
+const updateOneFieldTask = [
+
+];
+const deleteTask = [
+
+];
+module.exports = {
+  createTask,
+  getAllTasks,
+  getTaskById,
+  updateTask,
+  updateOneFieldTask,
+  deleteTask
+};
