e30262c12bda534dbc2dd1cecd7dc33218da8c41
santxjoya <109623621+santxjoya@users.noreply.github.com>
2025-02-26 20:40:35 -0500

    Update UserController.js

---
+++ src/controllers/UserController.js
@@ -0,0 +1,29 @@
+const User = require('../models/UserModel');
+const {Body, validatonResult}=require('express-validator');
+
+const createUser = [
+
+];
+const getAllUsers = [
+
+];
+const getUserById = [
+
+];
+const updateUser = [
+
+];
+const updateOneFieldUser = [
+
+];
+const deleteUser = [
+
+];
+module.exports = {
+  createUser,
+  getAllUsers,
+  getUserById,
+  updateUser,
+  updateOneFieldUser,
+  deleteUser
+};
